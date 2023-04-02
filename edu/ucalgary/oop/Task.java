f5e04a798b5c2efd1b91c7b8cd5f31b0a8b18fc5
adriannejulie <adriannejulia.lat@ucalgary.ca>
2023-04-02 06:39:45 -0600

    Attempted to implement vet exception and change of task, however more clarification is needed for requriements

--- edu/ucalgary/oop/Task.java
+++ edu/ucalgary/oop/Task.java
@@ -22,4 +22,56 @@ public class Task {
          this.startHour = startHour;
          this.animalID = animalID;
      }
+
+     /*
+      * getters and setters --maybe change docstring?
+      */
+
+    public void setTaskID (int id) {
+        this.taskID = id;
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+
+    public void setDuration (int duration) {
+        this.duration = duration;
+    }
+
+    public void setMaxWindow (int maxWindow) {
+        this.maxWindow = maxWindow;
+    }
+
+    public void setStartHour (int start) {
+        this.startHour = start;
+    }
+
+    public void setAnimalID (int id) {
+        this.animalID = id;
+    }
+
+    public int getTaskID() {
+        return this.taskID;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getDuration() {
+        return this.duration;
+    }
+
+    public int getMaxWindow() {
+        return this.maxWindow;
+    }
+
+    public int getStartHour() {
+        return this.startHour;
+    }
+
+    public int getAnimalID() {
+        return this.animalID;
+    }
 }
