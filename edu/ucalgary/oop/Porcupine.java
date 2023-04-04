c575c47848ce7461a74be64c6e0234b2e697e104
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2023-04-04 15:18:45 -0600

    Made thesame for these types, getters and constructors.

--- edu/ucalgary/oop/Porcupine.java
+++ edu/ucalgary/oop/Porcupine.java
@@ -1,8 +1,25 @@
 package edu.ucalgary.oop;
 
 public class Porcupine extends Animal{
-    private static int[] FEED_HOUR = {19, 20, 21};
-    private static int FOOD_PREP = 0;
-    private static int FEED_DURATION = 5;
-    private static int CLEAN_TIME = 10;
+    private static final int[] FEED_HOUR = {19, 20, 21};
+    private static final int FOOD_PREP = 0;
+    private static  final int FEED_DURATION = 5;
+    private static final  int CLEAN_TIME = 10;
+
+
+    public Porcupine(int animalID, String animalNickname){
+        super(animalID, animalNickname);
+    }
+    public  int[] getFeedHour(){
+        return FEED_HOUR;
+    }
+    public int getFoodPrep(){
+        return FOOD_PREP;
+    }
+    public int getFeedDuration(){
+        return FEED_DURATION;
+    }
+    public int getCleanTime(){
+        return CLEAN_TIME;
+    }
 }
